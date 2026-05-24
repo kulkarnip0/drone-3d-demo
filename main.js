a26f0589967743573d3d4205b422071f85e99a76
kulkarnip0 <praveen.anil.kulkarni@gmail.com>
2026-05-24 17:19:33 +0530

    Use import map paths for Three.js modules

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.1/build/three.module.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.164.1/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87ceeb);
