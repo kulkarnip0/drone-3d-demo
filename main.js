0641dddcafc834333d424480621881a3784345a0
kulkarnip0 <praveen.anil.kulkarni@gmail.com>
2026-05-26 20:57:59 +0530

    Publish simulation state to GCS dashboard

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ import { buildDronePaths } from "./src/drones/dronePaths.js";
 import { buildDroneFleet, updateDroneFleet } from "./src/drones/buildDroneFleet.js";
 import { setupAssetPlacement } from "./src/placeables/assetPlacement.js";
 import { buildDynamicObjects, updateDynamicObjects } from "./src/dynamicObjects/buildDynamicObjects.js";
+import { createSimulationStatePublisher } from "./src/state/simulationStatePublisher.js";
 
 const viewer = createViewer();
 const clock = new THREE.Clock();
@@ -16,11 +17,17 @@ const clock = new THREE.Clock();
 addLights(viewer.scene);
 const world = buildWorld(viewer.scene);
 buildMissionZones(viewer.scene);
-setupAssetPlacement(viewer, world);
+const assetPlacement = setupAssetPlacement(viewer, world);
 const dynamicObjects = buildDynamicObjects(viewer.scene);
 
 const paths = buildDronePaths(viewer.scene);
 const drones = buildDroneFleet(viewer.scene, paths);
+const statePublisher = createSimulationStatePublisher({
+  viewer,
+  drones,
+  dynamicObjects,
+  assetPlacement
+});
 
 setupCameraSwitching(
   viewer,
@@ -38,6 +45,7 @@ function animate() {
   updateSea(world.sea, elapsedTime);
   updateDroneFleet(drones, elapsedTime);
   updateDynamicObjects(dynamicObjects, elapsedTime);
+  statePublisher.publish(elapsedTime);
 
   viewer.renderer.render(viewer.scene, viewer.activeCamera);
 }
